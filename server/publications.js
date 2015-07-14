3692ba415744805a0e97a1ed2e67533b9c75532e
leejoon0 <leejoon0@gmail.com>
2015-07-14 08:41:53 +0000

    add files2

--- server/publications.js
+++ server/publications.js
@@ -1,16 +1,22 @@
-Meteor.publish('posts', function() {
-//Meteor.publish('allPosts', function() {
-  return Posts.find();
-  //return Posts.find({flagged: false, author: author);
+Meteor.publish('posts', function(options) {
+  check(options, {
+    sort: Object,
+    limit: Number
+  });
+  return Posts.find({}, options);
+});
+
+Meteor.publish('singlePost', function(id) {  
+  check(id, String)  
+  return Posts.find(id);
 });
 
 Meteor.publish('comments', function(postId) {
   check(postId, String);
   return Comments.find({postId: postId});
-  //return Comments.find();
 });
 
-Meteor.publish('notifications', function() {  
-  //return Notifications.find();
+Meteor.publish('notifications', function() {
   return Notifications.find({userId: this.userId, read: false});
 });
+
